7d8862ef3376cdf5ccecbb2d6b9684495809d118
Jairo Alves Cordeiro <jairoalves@hotmail.com>
2022-04-28 22:07:37 -0300

    recebe_nomes_entra21

---
+++ recebe_nomes_entra21/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace recebe_nomes_entra21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] nomes = new string[10];
+            
+            for (int i = 0; i < nomes.Length; i++)
+            {
+                Console.WriteLine("Insira um nome");
+                nomes[i] = Console.ReadLine();
+            }
+            //for (int i = 0; i < nomes.Length; i++)
+            //{
+
+            //    Console.WriteLine(nomes[i]);
+            }
+          }
+
+    }
+}
