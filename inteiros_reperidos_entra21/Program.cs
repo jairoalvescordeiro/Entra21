9bcb10077f00cdd51409f3d5ca65a03489472c49
Jairo Alves Cordeiro <jairoalves@hotmail.com>
2022-05-03 22:19:20 -0300

    Inteiros_reperidos_entra21

---
+++ inteiros_reperidos_entra21/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace inteiros_reperidos_entra21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Faça um programa que receba o número de entradas(as
+            //entradas devem ser de valores inteiros), mostre se há
+            //elementos repetidos e quantas vezes estes se repetem.
+
+            int valor;
+            int contador = 0;
+
+            Console.WriteLine("Digite um valor:");
+            valor = Convert.ToInt32(Console.ReadLine());
+            while (valor >= 1)
+            {
+                contador = contador + 1;
+                valor = Convert.ToInt32(Console.ReadLine());
+            }
+
+            int[] entradas = new int[valor];
+            int[] copia = new int[valor];
+            int[] contaRepetido = new int[valor];
+            int[] marcaRepetido = new int[valor];
+            
+            for (int i = 0; i < entradas.Length; i++)
+            {
+                entradas[i] = Convert.ToInt32(Console.ReadLine());
+                copia[i] = entradas[i];
+            }
+            for (int i = 0; i < entradas.Length; i++)
+                {
+                for (int j = 0; j < entradas.Length; j++) ;
+                
+            
+                    if (i != j)
+
+                        {
+               
+                        if (entradas[i] == copia[j] && marcaRepetido[j] == 0)
+               
+                }
+                    contaRepetido[i]++;
+                    marcaRepetido[i] = 1;
+                    marcaRepetido[j] = -1;
+                }
+                for (int i = 0; i < marcaRepetido.Length; i++);
+                    }
+            if (contaRepetidos[i] > 0 && marcaRepetidas[i] > 0)
+            {
+                Console.WriteLine($"O elemento {entradas[i]} na posição {i} repete {contaRepetidos[i]} vezes.");
+
+
+            }     }
+        }
+    
+    }
+}
--- teste/Program.cs
+++ teste/Program.cs
@@ -6,42 +6,76 @@ namespace teste
     {
         static void Main(string[] args)
         {
-            
-            float[] numeros = new float[10];
-            float menor = 0, maior = 0;
 
-            Console.WriteLine("Informe 10 números inteiros:");
-            numeros[i] = Convert.ToSingle(Console.ReadLine());
-            for (int i = 0; i < numeros; i++)
+            int vetor[150], vetor2[150][2],tam,i,k,aux,zero;
+            int main()
             {
-
-                Console.WriteLine((i + 1) + "º número:");
-                numeros[i] = Convert.ToSingle(Console.ReadLine());
-
-                if (i == 0)
+                printf("Digite o tamanho do vetor, no maximo 150 : ");
+                scanf("%d", &tam);
+                printf("Digite os valores do vetor: ");
+                for (i = 0; i < tam; i++)
                 {
-                    menor = numeros[0];
-                    maior = numeros[0];
+                    scanf("%d", &vetor[i]);
+                    vetor2[i][0] = vetor[i];
                 }
-
-                if (numeros[i] < menor)
+                //Ordenando o vetor
+                for (k = 0; k <= tam - 2; k++)
                 {
-
-                    menor = numeros[i];
-                    
+                    for (i = 0; i <= tam - 2; i++)
+                    {
+                        if (vetor[i] > vetor[i + 1])
+                        {
+                            aux = vetor[i];
+                            vetor[i] = vetor[i + 1];
+                            vetor[i + 1] = aux;
+                        }
+                    }
                 }
-                else if (numeros[i] > maior)
+                //Mostrando o vetor ordenado
+                printf("Vetor ordenado: \n");
+                for (i = 0; i < tam; i++)
                 {
-                    maior = numeros[i];
-
+                    printf("%d ", vetor[i]);
                 }
-
+                //Verificando numeros repetidos
+                for (i = 0; i < tam; i++)
+                {
+                    if (vetor2[i][0] == 0)
+                    {
+                        zero++;
+                    }
+                }
+                for (i = 0; i < tam; i++)
+                {
+                    for (k = 0; k < tam; k++)
+                    {
+                        if (i != k)
+                        {
+                            if (vetor2[i][0] == vetor2[k][0])
+                            {
+                                vetor2[k][0] = 0;
+                                vetor2[i][1]++;
+                            }
+                        }
+                    }
+                }
+                printf("\n\n");
+                if (zero > 0)
+                {
+                    if (zero == 1)
+                    {
+                        printf("O Numero 0 Saiu 1 Vez\n");
+                    }
+                    if (zero > 1)
+                    {
+                        printf("O Numero 0 Saiu %d Vezes\n", zero);
+                    }
+                }
+                for (i = 0; i < tam; i++)
+                {
+                    if (vetor2[i][0] != 0)
+                        printf("O Numero %d Saiu %d vezes\n", vetor2[i][0], (vetor2[i][1]) + 1);
+                }
+                getch();
+                return (1);
             }
-
-            Console.WriteLine("O menor número é:" + menor);
-            Console.WriteLine("O maior número é:" + maior);
-
-            Console.Read();
-        }
-    }
-}
