c0de3e7d268ac4df156b1ba7bf5e6765e67f146d
Jairo Alves Cordeiro <jairoalves@hotmail.com>
2022-05-06 10:46:11 -0300

    Atualizar

--- Calcular_motrar_vetores_entra21/Program.cs
+++ Calcular_motrar_vetores_entra21/Program.cs
@@ -11,15 +11,15 @@ namespace Calcular_mostra_vetores_entra21
             //a célula um do vetor “A” pela célula um do vetor “B” e assim
             //armazene o resultado em um terceiro vetor e mostre os resultados deste.
 
+
+            int[] vetor1 = new int[5];
+            int[] vetor2 = new int[5];
+            int[] vetor3 = new int[5];
             
-            int[] vetor1 = new int[2];
-            int[] vetor2 = new int[2];
-            int[] vetor3 = new int[2];
-                        
 
             for (int i = 0; i < vetor1.Length; i++)
             {
-                Console.WriteLine("Informe cinco números:");
+                Console.WriteLine("Informe números:");
                 vetor1[i] = Convert.ToInt32(Console.ReadLine());
             }
             for (int i = 0; i < vetor2.Length; i++)
@@ -28,8 +28,29 @@ namespace Calcular_mostra_vetores_entra21
                 vetor2[i] = Convert.ToInt32(Console.ReadLine());
             }
 
+            for (int i = 0; i < vetor1.Length; i++)
+            {
+                Console.WriteLine("Resultado A: " + vetor1[i]);
+                Console.WriteLine();//pula linha
+            }
 
-            
+            for (int i = 0; i < vetor2.Length; i++)
+            {
+                Console.WriteLine("Resultado B: " + vetor2[i]);
+                Console.WriteLine(); //pula linha
+            }
+
+            for (int i = 0; i < vetor3.Length; i++)
+            {
+                vetor3[i] = vetor1[i] * vetor2[i];
+            }
+            for (int i = 0; i < vetor3.Length; i++)
+            {
+                Console.WriteLine("Multiplicação entre A e B é: " + vetor3[i]);
+                Console.WriteLine();
+            }
+                
+
         }
     }
 }
